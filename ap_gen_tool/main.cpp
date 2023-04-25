e37daf24806e4eab6a96367e9bb6c7caf4bb3816
Daivuk <daivuk@gmail.com>
2023-04-25 16:46:46 -0400

    Fixed GPL headers missing

--- ap_gen_tool/main.cpp
+++ ap_gen_tool/main.cpp
@@ -1,3 +1,20 @@
+//
+// Copyright(C) 2023 David St-Louis
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//
+// *Generates python files for Archipelago, also C and C++ headers for APDOOM*
+//
+
 #include <stdio.h>
 #include <cinttypes>
 #include <string.h>
--- src/archipelago/apdoom.cpp
+++ src/archipelago/apdoom.cpp
@@ -1,3 +1,20 @@
+//
+// Copyright(C) 2023 David St-Louis
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//
+// *Source file for interfacing with archipelago*
+//
+
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
